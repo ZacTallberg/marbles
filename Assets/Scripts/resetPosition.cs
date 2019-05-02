703e38d3982b2dd938f8d98fb17b6eeff252cfeb
zcoberg <zcoberg@gmail.com>
2019-05-01 19:27:37 -0700

    first commit

---
+++ Assets/Scripts/resetPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetPosition : MonoBehaviour
+{
+
+    public void reset()
+    {
+        Vector3 temp = transform.position;
+        temp = new Vector3(6f, 0, 0);
+        transform.position = temp;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
